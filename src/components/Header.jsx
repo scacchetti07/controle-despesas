7fabd99f4e1428eeb9520c76f894a49c800a8f41
Scacchetti <luiscacchetti07@gmail.com>
2025-09-05 16:16:55 -0300

    feat: Adicionando modelo do chat (assets)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,14 @@ function Header() {
   return (
     <Hero>
       <header className="flex justify-between bg-slate-800/60 text-white w-full h-15 px-4">
-        <img src="../src/assets/money.png" className="p-2"/>
-        <nav className="flex items-center font-bold gap-10">
+        <img src="../src/assets/money.png" className="p-1"/>
+        <nav className="flex items-center font-bold gap-10 text-[18px]">
           <Link path="/">Home</Link>
           <Link path="/teste">Investidor</Link>
           <Link path="/contato">Contato</Link>
           <Link path="/sobre">Sobre</Link>
         </nav>
-        <button className="flex gap-2 border-2 border-white px-5 py-2 rounded-2xl hover:bg-white hover:text-black transition-colors duration-500 cursor-pointer my-auto font-bold">
+        <button className="flex gap-2 border-2 border-white px-5 py-1.5 rounded-2xl hover:bg-white hover:text-black transition-colors duration-500 cursor-pointer my-auto font-bold text-[18px]">
             <UserRound />
             Login
         </button>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hero({children}) {
   return (
-    <section className="w-full min-h-lvh bg-[url(../src/assets/heroImg1.jpg)] bg-slate-900 bg-center bg-no-repeat bg-cover">
+    <section className="w-full min-h-lvh bg-[url(../src/assets/heroImg1.jpg)] bg-slate-500 backdrop-brightness-50 bg-center bg-no-repeat bg-cover">
         {children}
         <div className='flex px-10 min-h-lvh items-center'>
             <h1 className='text-6xl text-white font-bold w-1/2 '>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
